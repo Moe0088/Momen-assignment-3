89e891e43742ecbccecb2ecd2c49d4d3ecc6c0cc
Moe0088 <you@example.com>
2024-04-11 15:14:05 -0400

    updated10

--- src/com/coderscampus/assignment3/RunnableApplication.java
+++ src/com/coderscampus/assignment3/RunnableApplication.java
@@ -6,49 +6,44 @@ import java.util.Scanner;
 public class RunnableApplication {
 
 	public static void main(String[] args) throws IOException {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		FileService fileService = new FileService();
-		User user = User();
-		fileService.readFile();
+
+		User[] users = fileService.readFile();
 
 		int attempts = 0;
-		boolean found = false;
-		while (!found) {
+
+		while (attempts < 5) {
+
 			System.out.println("Enter username: ");
-			String usernameInput = scanner.nextLine().toLowerCase();
+			String usernameInput = scan.nextLine().toLowerCase();
 			System.out.println("Enter password: ");
-			String passwordInput = scanner.nextLine();
+			String passwordInput = scan.nextLine();
+			attempts++;
+			validateUser(users, usernameInput, passwordInput);
 
-			if (user.getUsername().equalsIgnoreCase(usernameInput) && (user.getPassword().equals(passwordInput))) {
-
-				System.out.println("Welcome, " + user.getName() + "!");
-				found = true;
-
-			} else if ((user.getUsername() != usernameInput) && (user.getPassword() != passwordInput)) {
-
-				System.out.println("Invalid login, please try again");
-				attempts++;
-
-			}
-
-			{
-
-				if (attempts == 5) {
-					System.out.println("Too many failed login attempts, you are now locked out.");
-
-					break;
-				}
+			if (attempts == 5) {
+				System.out.println("Too many failed login attempts, you are now locked out.");
 
 			}
 
 		}
-
-		scanner.close();
+		scan.close();
 
 	}
 
-	private static User User() {
+	private static void validateUser(User[] users, String usernameInput, String passwordInput) {
+		for (User user : users) {
+			if (user.getUsername().equalsIgnoreCase(usernameInput) && (user.getPassword().equals(passwordInput))) {
 
-		return new User("username", "password", "name");
+				System.out.println("Welcome, " + user.getName() + "!");
+				break;
+
+			} else {
+
+				System.out.println("Invalid login, please try again");
+				break;
+			}
+		}
 	}
 }
